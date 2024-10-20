d125d0ca70c46f162bcd828c7bc91a3b56df8d48
Athul Bhandary <athulbhandary28@gmail.com>
2024-10-20 21:54:29 +0530

    minor updates

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -7,16 +7,19 @@ function Feed() {
     const [show,setShow]=useState(false)
 return (
     <div className="custom-cursor h-[600px] bg-[#c86cbc] p-20">
-        <p className="text-center text-5xl text-white">Feed Your Pet</p>
+        <p className="text-center text-5xl text-white underline">Feed Your Pet</p>
         <div 
             className="relative w-fit m-auto"
             onMouseEnter={() => setShow(true)}
             onMouseLeave={() => setShow(false)}
         >
             <img src={feedcat} alt="cat-img" className="h-96 m-auto" />
+            <div className="m-auto w-48 h-5 bg-pink-500 rounded-3xl">
+
+            </div>
             {show && (
                 <p className="absolute top-0 left-1/2 transform -translate-x-1/2 mt-2 font-bold text-black bg-white p-2 w-fit rounded-3xl">
-                    I want fish
+                    I want fish!!!
                 </p>
             )}
         </div>
